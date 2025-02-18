e941de6ffc66ac5301217e130d648f88652626bf
matheusdpo <matheusoliveira1991@hotmail.com>
2025-02-18 19:28:04 -0300

    request api

---
+++ src/main/java/com/lumen/api/v1/models/RequestBodyOcrModel.java
@@ -0,0 +1,22 @@
+package com.lumen.api.v1.models;
+
+public class RequestBodyOcrModel {
+    private String country;
+
+    private String documentType;
+
+    private String imageBase64;
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public String getImageBase64() {
+        return imageBase64;
+    }
+}
