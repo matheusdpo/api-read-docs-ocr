843bdb8ce7c55e1d18a24c0f8563cad7ef3a14d1
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-03 19:50:56 -0300

    env config

---
+++ src/main/java/com/lumen/api/v1/config/EnvConfig.java
@@ -0,0 +1,21 @@
+package com.lumen.api.v1.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Classe de configuração para variáveis de ambiente
+ */
+@Configuration
+public class EnvConfig {
+
+    /**
+     * Carrega as variáveis de ambiente do arquivo .env
+     */
+    public static void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+}
