11ecc12c47cc146f91483511887a8560bc902731
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-03 19:50:12 -0300

    load all env first

--- src/main/java/com/lumen/api/ApiApplication.java
+++ src/main/java/com/lumen/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.lumen.api;
 
+import com.lumen.api.v1.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		EnvConfig.loadEnv();
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
