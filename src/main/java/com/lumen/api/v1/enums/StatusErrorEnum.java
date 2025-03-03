95bea569f75a240b88bdb174f6d35497b57a7282
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-03 19:36:04 -0300

    new satus added

--- src/main/java/com/lumen/api/v1/enums/StatusErrorEnum.java
+++ src/main/java/com/lumen/api/v1/enums/StatusErrorEnum.java
@@ -5,7 +5,9 @@ public enum StatusErrorEnum {
     PARAMS_NULL_OR_EMPTY(1, "Params is null or empty"),
     COUNTRY_IS_NOT_AVAILABLE(2, "Country is not available yet"),
     DOC_IS_NOT_AVAILABLE(3, "Document type is not available"),
-    DOC_IS_NOT_VALID(4, "Document type is not valid for this country");
+    DOC_IS_NOT_VALID(4, "Document type is not valid for this country"),
+    DOC_MODEL_NOT_FOUND(5, "Document model not found"),
+    UNKOWN_ERROR(99, "Unknown error");
 
     StatusErrorEnum(Integer id, String errorMessage) {
         this.id = id;
