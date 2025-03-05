6558573e2953fa3a8a8affd19c40dd08b9304ef6
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-05 13:56:15 -0300

    renamed

--- api-service/src/main/java/com/lumen/api/v1/services/DocumentService.java
+++ api-service/src/main/java/com/lumen/api/v1/services/DocumentService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class DocumentServices {
+public class DocumentService {
 
     @Autowired
     private DocumentDBRepository documentDBRepository;
