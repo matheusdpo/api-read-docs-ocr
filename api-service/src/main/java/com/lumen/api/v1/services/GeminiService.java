991cc41e809986416283961851e7a32f06cc9fbf
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-18 15:58:50 -0300

    only gemini api uses

--- api-service/src/main/java/com/lumen/api/v1/services/GeminiService.java
+++ api-service/src/main/java/com/lumen/api/v1/services/GeminiService.java
@@ -1,32 +1,13 @@
 package com.lumen.api.v1.services;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.lumen.api.v1.exceptions.GeminiException;
-import com.lumen.api.v1.models.requests.Contents;
-import com.lumen.api.v1.models.requests.GeminiRequest;
-import com.lumen.api.v1.models.requests.Parts;
-import com.lumen.api.v1.models.responses.gemini.GeminiResponse;
-import com.lumen.api.v1.utils.SerializationUtils;
-import com.lumen.api.v1.utils.StringUtils;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
+import com.lumen.api.v1.utils.HttpUnirestUtils;
+import com.lumen.api.v1.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class GeminiService {
-
-    @Autowired
-    private SerializationUtils serializationUtils;
-
-    @Autowired
-    private StringUtils stringUtils;
-
-    @Autowired
-    private OcrSpaceService ocrSpaceService;
 
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
@@ -40,62 +21,22 @@ public class GeminiService {
     @Value("${gemini.api.v1.package}")
     private String geminiApiPackage;
 
+    @Autowired
+    private HttpUnirestUtils httpUnirestUtils;
 
-    public JsonNode getResponseBody(String base64, String docType, String country) throws Exception {
-        String docTypeModel = "Document" + docType + stringUtils.toCamelCase(country);
+    @Autowired
+    private JsonUtils jsonUtils;
 
-        if (!isClassExists(docTypeModel)) {
-            throw new GeminiException("Document Model Type not found");
-        }
-
-        String classModelStr = "";
-
-        try {
-            classModelStr = stringUtils.getClassNameStr(docTypeModel);
-        } catch (Exception e) {
-            throw new GeminiException("Impossible to get class name");
-        }
-
-        String text = ocrSpaceService.getString(base64);
-
-        String prompt = String.format(geminiApiPrompt, classModelStr, text);
-
-        String responseBody = getResponseBodyStr(prompt);
-
-        GeminiResponse geminiResponse = serializationUtils.fromJson2Class(responseBody, GeminiResponse.class);
-
-        return serializationUtils.serializeToJson(geminiResponse.getCandidates().getFirst().getContent().getParts().getFirst().getText());
+    public String getResponseBodyStr(String base64, String prompt, String mimeType) throws Exception {
+        String body = jsonUtils.createGeminiJson(base64, prompt, mimeType);
+        return httpUnirestUtils.post(geminiApiUrl, geminiApiKey, body).getBody();
     }
 
-    public boolean isClassExists(String className) {
-        try {
-            Class.forName(geminiApiPackage + className);
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
+    public String getGeminiApiPackage() {
+        return this.geminiApiPackage;
     }
 
-    public String getResponseBodyStr(String prompt) throws GeminiException {
-        try {
-            GeminiRequest responseModel = new GeminiRequest();
-            Contents contents = new Contents();
-            Parts parts = new Parts();
-
-            parts.setText(prompt);
-            contents.setParts(List.of(parts));
-            responseModel.setContents(List.of(contents));
-
-            String fullUrl = geminiApiUrl + geminiApiKey;
-
-            HttpResponse<String> response = Unirest.post(fullUrl)
-                    .header("Content-Type", "application/json")
-                    .body(responseModel)
-                    .asString();
-
-            return response.getBody();
-        } catch (Exception e) {
-            throw new GeminiException("Impossible to get response from API Docs");
-        }
+    public String getGeminiApiPrompt() {
+        return this.geminiApiPrompt;
     }
 }
