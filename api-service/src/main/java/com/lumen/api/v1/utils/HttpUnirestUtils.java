2eac5811869b32106c2cec9ef925f70e7a5255a3
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-18 16:01:58 -0300

    http unirest utils done

---
+++ api-service/src/main/java/com/lumen/api/v1/utils/HttpUnirestUtils.java
@@ -0,0 +1,21 @@
+package com.lumen.api.v1.utils;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpUnirestUtils {
+
+    public HttpResponse<String> post(String url, String apiKey, String body) throws UnirestException {
+        return post(url + apiKey, body);
+    }
+
+    public HttpResponse<String> post(String url, String body) throws UnirestException {
+        return Unirest.post(url)
+                .header("Content-Type", "application/json")
+                .body(body)
+                .asString();
+    }
+}
