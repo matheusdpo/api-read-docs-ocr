547efc47bc17692d66236698b26f5f419c0e2f43
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-20 15:48:46 -0300

    doc size limit

--- api-service/src/main/java/com/lumen/api/v1/services/DocumentService.java
+++ api-service/src/main/java/com/lumen/api/v1/services/DocumentService.java
@@ -3,6 +3,7 @@ package com.lumen.api.v1.services;
 import com.lumen.api.v1.entities.DocumentDBEntity;
 import com.lumen.api.v1.repositories.DocumentDBRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,11 +14,18 @@ public class DocumentService {
     @Autowired
     private DocumentDBRepository documentDBRepository;
 
+    @Value("${document.size.max}")
+    private Long documentSizeMaxMb;
+
     public boolean isDocumentTypeAndCountry(String documentType, String country) {
         return findByDocumentTypeAndCountry(documentType, country).isPresent();
     }
 
     public Optional<DocumentDBEntity> findByDocumentTypeAndCountry(String documentType, String country) {
         return documentDBRepository.findByDoctypeAndCountry(documentType, country);
+    }
+
+    public boolean isDocumentSizeValid(Long size) {
+        return size <= documentSizeMaxMb;
     }
 }
