a7f3b012dc44f712531ad4b9af1adb6c45131b53
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-20 15:50:10 -0300

    app

--- auth-service/src/main/java/com/lumen/auth/AuthApplication.java
+++ auth-service/src/main/java/com/lumen/auth/AuthApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthApplication {
 
-	public static void main(String[] args) {
-		EnvConfig.loadEnv();
-		SpringApplication.run(AuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        EnvConfig.loadEnv();
+        SpringApplication.run(AuthApplication.class, args);
+    }
 
 }
