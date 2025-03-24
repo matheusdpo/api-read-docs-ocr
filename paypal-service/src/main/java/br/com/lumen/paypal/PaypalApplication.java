b0604bce884e8badad93110bdef4c131c6da35c3
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-24 14:59:48 -0300

    main

---
+++ paypal-service/src/main/java/br/com/lumen/paypal/PaypalApplication.java
@@ -0,0 +1,15 @@
+package br.com.lumen.paypal;
+
+import br.com.lumen.paypal.v1.config.EnvConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalApplication {
+
+    public static void main(String[] args) {
+        EnvConfig.loadEnv();
+        SpringApplication.run(PaypalApplication.class, args);
+    }
+
+}
