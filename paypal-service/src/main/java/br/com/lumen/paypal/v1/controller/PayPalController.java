ecfabcda021b492d81f37c968d7ba596d049e222
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-24 14:59:53 -0300

    controller added

---
+++ paypal-service/src/main/java/br/com/lumen/paypal/v1/controller/PayPalController.java
@@ -0,0 +1,9 @@
+package br.com.lumen.paypal.v1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/paypal")
+public class PayPalController {
+}
