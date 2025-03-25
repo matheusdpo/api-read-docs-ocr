3a2ed33127f3fbaa014b53fa5ceeb1e5f391d0bb
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-25 05:08:43 -0300

    config paypal

---
+++ paypal-service/src/main/java/br/com/lumen/paypal/v1/config/PayPalConfig.java
@@ -0,0 +1,24 @@
+package br.com.lumen.paypal.v1.config;
+
+import com.paypal.base.rest.APIContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PayPalConfig {
+
+    @Value("${paypal.clientid}")
+    private String clientId;
+
+    @Value("${paypal.clientsecret}")
+    private String clientSecret;
+
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public APIContext apiContext() {
+        return new APIContext(clientId, clientSecret, mode);
+    }
+}
