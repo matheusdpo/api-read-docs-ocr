6c6eaa6c37c19b3450f245bfbbede7a9d1ceb790
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-25 23:50:00 -0300

    removed completed status enum

--- paypal-service/src/main/java/com/lumen/paypal/v1/enums/PaymentStatusEnum.java
+++ paypal-service/src/main/java/com/lumen/paypal/v1/enums/PaymentStatusEnum.java
@@ -3,12 +3,11 @@ package com.lumen.paypal.v1.enums;
 public enum PaymentStatusEnum {
 
     CREATED("CREATED"),
-    APPROVED("APPROVED"),
     DECLINED("DECLINED"),
     CANCELLED("CANCELLED"),
     ERROR("ERROR"),
     REFUNDED("REFUNDED"),
-    COMPLETED("COMPLETED");
+    APPROVED("APPROVED");
 
     String paymentStatus;
 
