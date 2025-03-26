4343867942294abfa7a557eebde70c8be17d30cb
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-26 17:24:39 -0300

    add logs db

--- api-service/src/main/java/com/lumen/api/v1/controller/OcrController.java
+++ api-service/src/main/java/com/lumen/api/v1/controller/OcrController.java
@@ -6,6 +6,7 @@ import com.lumen.api.v1.models.responses.api.ResponseErrorApiModel;
 import com.lumen.api.v1.services.ApiKeyService;
 import com.lumen.api.v1.services.DocumentService;
 import com.lumen.api.v1.services.LumenAPIService;
+import com.lumen.api.v1.services.RequestLogsService;
 import com.lumen.api.v1.utils.Base64Utils;
 import com.lumen.api.v1.utils.LogUtils;
 import jakarta.validation.constraints.NotNull;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Controller
@@ -36,14 +38,21 @@ public class OcrController {
     private ApiKeyService apiKeyService;
 
     @Autowired
+    private RequestLogsService requestLogsService;
+
+    @Autowired
     private Base64Utils base64Utils;
 
     @Autowired
     private LogUtils logger;
 
+    private static final String ENDPOINT = "/api/v1/ocr";
+
     @PostMapping("/ocr")
     public ResponseEntity<?> init(@RequestHeader(value = "X-API-KEY", required = false) String apiKey,
                                   @RequestPart("file") @NotNull MultipartFile file) {
+        LocalDateTime requestDate = LocalDateTime.now();
+
         try {
             if (Objects.isNull(apiKey) || apiKey.isBlank()) {
                 return buildErrorResponse(StatusErrorEnum.API_KEY_IS_REQUIRED, HttpStatus.BAD_REQUEST);
@@ -78,11 +87,26 @@ public class OcrController {
                     mimeType
             );
 
+            HttpStatus httpStatusCode = HttpStatus.OK;
+
+            requestLogsService.saveLog(apiKey,
+                    ENDPOINT,
+                    httpStatusCode,
+                    requestDate
+            );
+
             return ResponseEntity
-                    .status(HttpStatusCode.valueOf(200))
+                    .status(httpStatusCode)
                     .body(response);
         } catch (Exception e) {
             logger.error("Error in ControllerApi", e);
+            HttpStatusCode httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+
+            requestLogsService.saveLog(apiKey,
+                    ENDPOINT,
+                    httpStatusCode,
+                    requestDate
+            );
             return buildErrorResponse(StatusErrorEnum.UNKOWN_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
