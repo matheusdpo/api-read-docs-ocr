69340030cfa802cf932f71d063a63ce6fddefe28
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-25 23:52:51 -0300

    added new method to find keys using status and update

--- auth-service/src/main/java/com/lumen/auth/v1/repositories/ApiKeyRepository.java
+++ auth-service/src/main/java/com/lumen/auth/v1/repositories/ApiKeyRepository.java
@@ -3,7 +3,11 @@ package com.lumen.auth.v1.repositories;
 import com.lumen.auth.v1.entities.ApiKeyEntity;
 import com.lumen.auth.v1.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,13 +15,14 @@ import java.util.Optional;
 /**
  * Repository for ApiKeyEntity
  * @author matheusdpo
- * @since 2025-03
  * @version 1.0.0
+ * @since 2025-03
  */
 @Repository
 public interface ApiKeyRepository extends JpaRepository<ApiKeyEntity, String> {
     /**
      * Find all {@link ApiKeyEntity} by {@link UserEntity}
+     *
      * @param userEntity - {@link UserEntity} to find {@link ApiKeyEntity} by it
      * @return List of {@link ApiKeyEntity}
      */
@@ -25,8 +30,37 @@ public interface ApiKeyRepository extends JpaRepository<ApiKeyEntity, String> {
 
     /**
      * Find {@link ApiKeyEntity} by key
+     *
      * @param key - key to find ApiKeyEntity by it
      * @return {@link ApiKeyEntity} - ApiKeyEntity found by key
      */
     Optional<ApiKeyEntity> findByKey(String key);
+
+    /**
+     * Find all {@link ApiKeyEntity} by {@link UserEntity} and status key
+     * @param userEntity - {@link UserEntity} to find {@link ApiKeyEntity} by it
+     * @param statusKey - status key to find {@link ApiKeyEntity} by it
+     * @return List of {@link ApiKeyEntity}
+     */
+    List<ApiKeyEntity> findByUserEntityAndStatusKey(UserEntity userEntity, String statusKey);
+
+    /**
+     * Update the status of an API key
+     *
+     * @param key       - The API key to update
+     * @param statusKey - The new status
+     */
+    @Modifying
+    @Transactional
+    @Query("UPDATE ApiKeyEntity a SET a.statusKey = :statusKey WHERE a.key = :key")
+    void updateStatusKey(@Param("key") String key, @Param("statusKey") String statusKey);
+
+
+    /**
+     * Find all {@link ApiKeyEntity} by {@link UserEntity} and status key
+     * @param userEntity - {@link UserEntity} to find {@link ApiKeyEntity} by it
+     * @param statusKey - status key to find {@link ApiKeyEntity} by it
+     * @return List of {@link ApiKeyEntity}
+     */
+    List<ApiKeyEntity> findAllByUserEntityAndStatusKey(UserEntity userEntity, String statusKey);
 }
