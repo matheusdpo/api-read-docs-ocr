45aca44297603e655537639f25b8ecd42c995704
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-27 20:34:03 -0300

    added annotation repository

--- api-service/src/main/java/com/lumen/api/v1/repositories/DocumentDBRepository.java
+++ api-service/src/main/java/com/lumen/api/v1/repositories/DocumentDBRepository.java
@@ -4,9 +4,11 @@ import com.lumen.api.v1.entities.DocumentDBEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DocumentDBRepository extends JpaRepository<DocumentDBEntity, Long> {
 
     @Query("SELECT d FROM DocumentDBEntity d WHERE d.documentType = :docType AND d.country = :country")
