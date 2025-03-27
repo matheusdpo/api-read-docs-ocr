db8f0e31e60d909e052ce671abf12ff3e593ad52
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-27 20:39:01 -0300

    annotation repository added

--- paypal-service/src/main/java/com/lumen/paypal/v1/repositories/PaymentsRepository.java
+++ paypal-service/src/main/java/com/lumen/paypal/v1/repositories/PaymentsRepository.java
@@ -2,10 +2,12 @@ package com.lumen.paypal.v1.repositories;
 
 import com.lumen.paypal.v1.entities.PaymentsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PaymentsRepository extends JpaRepository<PaymentsEntity, Long> {
     List<PaymentsEntity> findAllByUserId(Long userId);
 
