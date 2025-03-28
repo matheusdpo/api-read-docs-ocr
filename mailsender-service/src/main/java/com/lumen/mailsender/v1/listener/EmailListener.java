834a0eb637035698600df5c416e8da0086cf2891
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-28 01:25:22 -0300

    listener rabbitmq

---
+++ mailsender-service/src/main/java/com/lumen/mailsender/v1/listener/EmailListener.java
@@ -0,0 +1,35 @@
+package com.lumen.mailsender.v1.listener;
+
+import com.lumen.mailsender.v1.models.InvoiceMessageModel;
+import com.lumen.mailsender.v1.service.EmailService;
+import com.lumen.mailsender.v1.utils.LogUtils;
+import jakarta.mail.MessagingException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailListener {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private LogUtils logger;
+
+    @RabbitListener(queues = "rabbitmq.queue.invoice")
+    public void sendEmailInvoice(InvoiceMessageModel invoiceMessageModel) {
+        try {
+            emailService.sendMailInvoice(invoiceMessageModel);
+        } catch (MessagingException e) {
+            logger.error("Error sending email invoice", e);
+        }
+    }
+
+    @RabbitListener(queues = "rabbitmq.queue.reminder")
+    public void sendEmailReminder(InvoiceMessageModel invoiceMessageModel) throws MessagingException {
+        try {
+            emailService.sendMailReminder(invoiceMessageModel);
+        } catch (MessagingException e) {
+            logger.error("Error sending email invoice", e);
+        }
+    }
+}
