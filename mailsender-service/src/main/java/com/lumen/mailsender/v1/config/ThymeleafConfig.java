e2c46b78218de432949922676bf7eb57fe5dfcc5
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-28 01:26:37 -0300

    thymeleaf config

---
+++ mailsender-service/src/main/java/com/lumen/mailsender/v1/config/ThymeleafConfig.java
@@ -0,0 +1,23 @@
+package com.lumen.mailsender.v1.config;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+public class ThymeleafConfig {
+
+    public static TemplateEngine emailTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/"); // Pasta onde estão seus templates
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false);
+
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine;
+    }
+}
