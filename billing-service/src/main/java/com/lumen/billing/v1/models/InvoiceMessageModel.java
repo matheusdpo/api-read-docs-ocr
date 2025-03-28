e0baf7b4a3e677d3cbc48ccaf6e0b6802bcf1d7b
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-28 01:25:59 -0300

    model invoice message

---
+++ billing-service/src/main/java/com/lumen/billing/v1/models/InvoiceMessageModel.java
@@ -0,0 +1,70 @@
+package com.lumen.billing.v1.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class InvoiceMessageModel {
+    private String name;
+    private String lastName;
+    private String email;
+    private BigDecimal totalAmount;
+    private String description;
+    private LocalDate dueDate;
+    private String invoiceNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+}
