de48d036dc1e0c36356cb5c34b07d278ad649b3b
matheusdpo <matheusoliveira1991@hotmail.com>
2025-03-28 01:42:26 -0300

    ENV CONFIG

--- mailsender-service/src/main/java/com/lumen/mailsender/MailsenderApplication.java
+++ mailsender-service/src/main/java/com/lumen/mailsender/MailsenderApplication.java
@@ -1,13 +1,15 @@
 package com.lumen.mailsender;
 
+import com.lumen.mailsender.v1.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MailsenderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailsenderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        EnvConfig.loadEnv();
+        SpringApplication.run(MailsenderApplication.class, args);
+    }
 
 }
