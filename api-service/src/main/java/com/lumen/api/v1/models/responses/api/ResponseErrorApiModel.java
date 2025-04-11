08dc3c961d1eb04ddf7aa32683e56a2c5e094b1b
matheus <matheusoliveira1991@hotmail.com>
2025-04-11 01:50:12 -0300

    changed to record

--- api-service/src/main/java/com/lumen/api/v1/models/responses/api/ResponseErrorApiModel.java
+++ api-service/src/main/java/com/lumen/api/v1/models/responses/api/ResponseErrorApiModel.java
@@ -1,24 +1,10 @@
 package com.lumen.api.v1.models.responses.api;
 
-public class ResponseErrorApiModel {
-    private final boolean isError = true;
-    private Integer status;
-    private String message;
+import java.time.LocalDateTime;
 
-    public ResponseErrorApiModel(Integer status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+public record ResponseErrorApiModel(
+        LocalDateTime timestamp,
+        String message,
+        String detail) {
 
-    public boolean isError() {
-        return isError;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
